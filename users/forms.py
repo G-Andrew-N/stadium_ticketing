0bb69e3127d0f601d293e36658292f424b43f17b
Andrew <dreandrew11091@gmail.com>
2026-02-25 19:06:47 +0300

    Remove username field from CustomUserCreationForm and registration template for streamlined user registration process

--- users/forms.py
+++ users/forms.py
@@ -7,11 +7,16 @@ class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
-    photo = forms.ImageField(required=False)  # This is fine
+    photo = forms.ImageField(required=False)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2', 'photo']
+        fields = ['email', 'first_name', 'last_name', 'password1', 'password2', 'photo']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if 'username' in self.fields:
+            self.fields.pop('username')
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
